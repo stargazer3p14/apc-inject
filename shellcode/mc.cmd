72deff0cdf1314f41e4d0a98796f57b7c86ade5f
stargazer3p14 <83684429+stargazer3p14@users.noreply.github.com>
2026-03-23 17:04:00 +0200

    Correct mc.cmd

--- shellcode/mc.cmd
+++ shellcode/mc.cmd
@@ -5,7 +5,7 @@ ml64 shellcode-simple-msgbox.asm /link /entry:AlignRSP /force:multiple
 dump-text shellcode-simple-msgbox.exe shellcode-simple-msgbox.bin
 
 :: Copy to default shellcode name "shellcode1.txt"
-:: copy /y shellcode-simple-msgbox.bin shellcode1.txt
+copy /y shellcode-simple-msgbox.bin shellcode1.txt
 
 ::
 :: shellcode-msgbox-no-params.bin -- pops up MessageBox() and two consoles, runs two threads and uses no params
@@ -15,4 +15,4 @@ ml64 shellcode-msgbox-no-params.asm /link /entry:AlignRSP /force:multiple
 dump-text shellcode-msgbox-no-params.exe shellcode-msgbox-no-params.bin
 
 :: Copy to default shellcode name "shellcode1.txt"
-copy /y shellcode-msgbox-no-params.bin shellcode1.txt
+::copy /y shellcode-msgbox-no-params.bin shellcode1.txt
